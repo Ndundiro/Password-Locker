26afe9c9ce19714b64bc24124508ae0677cf1cf2
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-09-01 22:58:04 +0300

    To check if credentials once entered can be saved.

--- credentials.py
+++ credentials.py
@@ -9,3 +9,17 @@ class Credentials:
         self.login_detail = login_detail
         self.Password = Password
         ''' save our accounts passwords here.'''
+
+        def test_init(self):
+        
+            ## check if initialization happens as expected
+        
+            self.assertEqual(self.new_credential_account_name, "Facebook",)
+            self.assertEqual(self.new_credential_login_detail, "0712345678")
+            self.assertEqual(self.new_credential_Password, "alfiecode")
+
+        # check if saving multiple cridentials is possible
+        def test_save_credentials(self):
+  
+        self.new_cred.save_cred()
+        self.assertEqual(len(Credentials.cred_list),1)
