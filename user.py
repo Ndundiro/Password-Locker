b6bfbba92911128fa2ee4d75ea58f72ba3dfcc1b
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-09-01 19:30:40 +0300

    Instantiates our class user with the expected cridentials.

---
+++ user.py
@@ -0,0 +1,16 @@
+import pyperclip
+class User:
+
+
+    '''
+    Class that generates new instances of user
+    '''
+    user_list = []  # list of users to be stored here
+
+    def __init__(self, user_name, email, password):
+        '''
+        saving user credentials into user_list for login
+        '''
+        self.user_name = user_name
+        self.email = email
+        self.password = password
