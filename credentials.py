90cfccc94694dc8ce4a21bc5c6054df4520ea683
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-09-01 23:09:20 +0300

    Adds a test to check if the credentials can be saves.

--- credentials.py
+++ credentials.py
@@ -10,16 +10,14 @@ class Credentials:
         self.Password = Password
         ''' save our accounts passwords here.'''
 
-        def test_init(self):
         
-            ## check if initialization happens as expected
-        
-            self.assertEqual(self.new_credential_account_name, "Facebook",)
-            self.assertEqual(self.new_credential_login_detail, "0712345678")
-            self.assertEqual(self.new_credential_Password, "alfiecode")
 
         # check if saving multiple cridentials is possible
-        def test_save_credentials(self):
-  
-        self.new_cred.save_cred()
-        self.assertEqual(len(Credentials.cred_list),1)
+       
+
+        
+def save_cred(self):
+        '''
+        self credentials in cred_list
+        '''
+        Credentials.cred_list.append(self)
--- credentials_test.py
+++ credentials_test.py
@@ -15,3 +15,20 @@ class TestCredentials(unittest.TestCase):
     #     clear list before any test is run
     #     '''
     #     Credentials.credentials_list = []
+
+    def test_init(self):
+        
+            ## check if initialization happens as expected
+        
+            self.assertEqual(self.new_credential_account_name, "Facebook",)
+            self.assertEqual(self.new_credential_login_detail, "0712345678")
+            self.assertEqual(self.new_credential_Password, "alfiecode")
+
+    
+    #check if our credentials can be saved
+    def test_save_credentials(self):
+        '''
+        check if credentials can be saved
+        '''  
+        self.new_cred.save_cred()
+        self.assertEqual(len(Credentials.cred_list),1)
